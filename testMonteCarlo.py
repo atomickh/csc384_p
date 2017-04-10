71d68b513fc7a902ec98880a87ab75badd57272e
Kevin Hua <Kevin Hua>
2017-04-09 21:30:50 -0400

    Last part

--- testMonteCarlo.py
+++ testMonteCarlo.py
@@ -12,24 +12,9 @@ class TestGetAction(unittest.TestCase):
         self.ghostType = loadAgent('RandomGhost', nographics=True)
         self.rules.quiet = False
 
-    def testSingleMove(self):
-        map = layout.tryToLoad('test_cases/stuckClassic.lay')
-        pacman = MonteCarloAgent()
-        game = self.rules.newGame(map, pacman, [], textDisplay.NullGraphics(), True, True)
-        state = game.state
-        pacman.getAction(state)
-        self.assertEqual(len(pacman.plays), 0)
-
     def testTimeOut(self):
-        map = layout.getLayout('smallClassic')
         pacman = MonteCarloAgent()
-        ghosts = [self.ghostType(i + 1) for i in range(2)]
-        game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
-        state = game.state
-        begin = datetime.datetime.utcnow()
-        pacman.getAction(state)
-        total = datetime.datetime.utcnow() - begin
-        self.assertAlmostEqual(total, datetime.timedelta(milliseconds=50), delta=datetime.timedelta(milliseconds=400))
+        self.assertEqual(pacman.calculation_time, datetime.timedelta(milliseconds=50))
 
     def testSelection(self):
         map = layout.getLayout('testClassic')
@@ -43,8 +28,10 @@ class TestGetAction(unittest.TestCase):
         stud_act = pacman.getAction(state)
         actions = state.getLegalActions(0)
         max_val = max(float(pacman.wins[state.generateSuccessor(0, a)]) / pacman.plays[state.generateSuccessor(0, a)]
-                      for a in actions if state.generateSuccessor(0, a) in pacman.plays)
-        max_acts = [a for a in actions if state.generateSuccessor(0, a) in pacman.plays and
+                      for a in actions if state.generateSuccessor(0, a) in pacman.plays
+                      and pacman.plays[state.generateSuccessor(0, a)] != 0)
+        max_acts = [a for a in actions if state.generateSuccessor(0, a) in pacman.plays
+                    and pacman.plays[state.generateSuccessor(0, a)] != 0 and
                     float(pacman.wins[state.generateSuccessor(0, a)]) /
                     pacman.plays[state.generateSuccessor(0, a)] == max_val]
         self.assertIn(stud_act, max_acts)
@@ -62,6 +49,8 @@ class TestRunSimulation(unittest.TestCase):
         ghosts = [self.ghostType(i + 1) for i in range(1)]
         game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
         state = game.state
+        pacman.plays[state] = 1
+        pacman.wins[state] = 0
         states = [state]
         for i in range(2):
             states = [s.generateSuccessor(i, a) for s in states for a in s.getLegalActions(i)]
@@ -69,9 +58,9 @@ class TestRunSimulation(unittest.TestCase):
                 pacman.plays[s] = 1
                 pacman.wins[s] = 0
         # If pacman searches to correct depth after expansion, will result in one state getting added to plays.
-        prev = len(pacman.plays)
+        prev = len([s for s in pacman.plays if pacman.plays[s] != 0])
         pacman.run_simulation(state)
-        self.assertEqual(prev + 1, len(pacman.plays))
+        self.assertEqual(prev + 1, len([s for s in pacman.plays if pacman.plays[s] != 0]))
 
     def testExpansion(self):
         map = layout.getLayout('smallClassic')
@@ -81,19 +70,21 @@ class TestRunSimulation(unittest.TestCase):
         state = game.state
         for i in range(2):
             pacman.run_simulation(state)
-        self.assertEqual(len(pacman.plays), 2)
+        self.assertEqual(len([s for s in pacman.plays if pacman.plays[s] != 0]), 2)
 
-    def earlyWin(self):
+    def testEarlyWin(self):
         map = layout.tryToLoad('test_cases/shortClassic.lay')
         pacman = MonteCarloAgent()
         ghosts = [self.ghostType(i + 1) for i in range(1)]
         game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
         state = game.state
         actions = state.getLegalActions(0)
+        pacman.plays[state] = len(actions) + 1
+        pacman.wins[state] = len(actions)
         # Run it once for every successor.
         for a in actions:
             succ = state.generateSuccessor(0, a)
-            if a == Directions.EAST:
+            if a == Directions.WEST:
                 pacman.plays[succ] = 1
                 pacman.wins[succ] = 1
             else:
@@ -108,15 +99,21 @@ class TestRunSimulation(unittest.TestCase):
         ghosts = [self.ghostType(i + 1) for i in range(1)]
         game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
         state = game.state
-        pacman.plays[state] = 1
-        pacman.wins[state] = 0
         succ = [state.generateSuccessor(0, a) for a in state.getLegalActions(0)]
+        pacman.plays[state] = len(succ)
+        pacman.wins[state] = len(succ)
         for s in succ[1:len(succ)]:
             pacman.plays[s] = 1
             pacman.wins[s] = 0
-        pacman.run_simulation(state)
-        # Should play the single state without information.
-        self.assertEqual(pacman.plays[succ[0]], 1)
+        try:
+            pacman.run_simulation(state)
+        except KeyError:
+            # Using method where expand all successor states and set wins and plays to 0
+            pacman.plays[succ[-1]] = 0
+            pacman.wins[succ[-1]] = 0
+            pacman.run_simulation(state)
+        # Should play the single state without information
+        self.assertEqual(pacman.plays[succ[-1]], 1)
 
     def testUCB1(self):
         map = layout.getLayout('smallClassic')
@@ -125,6 +122,8 @@ class TestRunSimulation(unittest.TestCase):
         game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
         state = game.state
         succ = [state.generateSuccessor(0, a) for a in state.getLegalActions(0)]
+        pacman.plays[state] = len(succ) + 1
+        pacman.wins[state] = len(succ)
         pacman.plays[succ[0]] = 1
         pacman.wins[succ[0]] = 1
         for i in range(1, len(succ)):
@@ -140,6 +139,8 @@ class TestRunSimulation(unittest.TestCase):
         game = self.rules.newGame(map, pacman, ghosts, textDisplay.NullGraphics(), True, True)
         state = game.state
         succ = [state.generateSuccessor(0, a) for a in state.getLegalActions(0)]
+        pacman.plays[state] = len(succ) + 2
+        pacman.wins[state] = len(succ) + 2
         pacman.plays[succ[0]] = 1
         pacman.wins[succ[0]] = 0
         for i in range(1, len(succ)):
